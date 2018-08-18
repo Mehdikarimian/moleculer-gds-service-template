0f2ab5f3c1c4fc8779041b2d4942df51c1a59cd0
Hamed Torbatian <hamed.tm@gmail.com>
2018-08-18 11:59:50 +0430

    init template

---
+++ meta.js
@@ -0,0 +1,19 @@
+module.exports = function (values) {
+  return {
+    questions: [],
+
+    metalsmith: {},
+
+    // skipInterpolation: [],
+
+    filters: {},
+
+    completeMessage: `
+To get started:
+
+	cd {{projectName}}
+    npm run dev
+
+		`
+  }
+}
---
+++ template/config/config.prod.js
@@ -0,0 +1,11 @@
+module.exports = {
+  db: {
+    host: 'mongodb://127.0.0.1',
+    port: '27017',
+    name: '{{projectName}}'
+  },
+  logger: {
+    host: '127.0.0.1',
+    port: 9988
+  }
+}
